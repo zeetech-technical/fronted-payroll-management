9dcf70e344e105e7b765cc32c9ca2e86658fac27
VictorN809 <nievesv809@gmail.com>
2026-05-20 01:26:33 -0600

    feat(config vite for internal proxy docker)

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    server: {
+    host: '0.0.0.0',
+    proxy: {
+      '/api/v1': {
+        target: 'http://backend:3000',
+        changeOrigin: true,
+      },
+    },
+  },
 })
